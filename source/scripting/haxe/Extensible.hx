89e60eea5c5dd1779684a901cbed7ef6b2a547a2
AlejoGDOfficial <alejogdoff@gmail.com>
2025-10-01 20:57:09 -0500

    Extensible

--- source/scripting/haxe/Extensible.hx
+++ source/scripting/haxe/Extensible.hx
@@ -3,57 +3,58 @@ package scripting.haxe;
 import rulescript.scriptedClass.RuleScriptedClass;
 
 import flixel.*;
-import flixel.ui.*;
 import flixel.util.*;
 import flixel.text.*;
 import flixel.math.*;
 import flixel.group.*;
-import flixel.sound.*;
+import flixel.ui.*;
 import flixel.graphics.*;
-import flixel.addons.ui.*;
 import flixel.addons.display.*;
 
-import openfl.display.Sprite;
-import openfl.text.TextField;
+import openfl.display.*;
+import openfl.text.*;
 
-import scripting.lua.LuaPresetBase;
+import scripting.lua.*;
+
+import funkin.visuals.*;
+import funkin.visuals.objects.*;
+import funkin.visuals.mobile.*;
 
 private typedef FlxDrawItem = flixel.graphics.tile.FlxDrawQuadsItem;
 
 class Extensible {}
 
+// Flixel
+
+class ScriptBasic extends FlxBasic implements RuleScriptedClass {}
 class ScriptObject extends FlxObject implements RuleScriptedClass {}
-
-class ScriptSprite extends FlxSprite implements RuleScriptedClass {}
-
-class ScriptText extends FlxText implements RuleScriptedClass {}
-
 class ScriptGroup extends FlxGroup implements RuleScriptedClass {}
-
 class ScriptSpriteGroup extends FlxSpriteGroup implements RuleScriptedClass {}
 
 class ScriptTimer extends FlxTimer implements RuleScriptedClass {}
-
 class ScriptSound extends FlxSound implements RuleScriptedClass {}
-
 class ScriptRect extends FlxRect implements RuleScriptedClass {}
 
 class ScriptButton extends FlxButton implements RuleScriptedClass {}
-
 class ScriptBar extends FlxBar implements RuleScriptedClass {}
-
 class ScriptGraphic extends FlxGraphic implements RuleScriptedClass {}
 
-class ScriptBasic extends FlxBasic implements RuleScriptedClass {}
-
+class ScriptSprite extends FlxSprite implements RuleScriptedClass {}
+class ScriptBackdrop extends FlxBackdrop implements RuleScriptedClass {}
 class ScriptRuntimeShader extends FlxRuntimeShader implements RuleScriptedClass {}
+
+class ScriptText extends FlxText implements RuleScriptedClass {}
+class ScriptBitmapText extends FlxBitmapText implements RuleScriptedClass {}
+class ScriptTextFormat extends FlxTextFormat implements RuleScriptedClass {}
 
 class ScriptCamera extends FlxCamera implements RuleScriptedClass {}
 
+// OpenFL
+
+@:forceOverride class ScriptOpenFLSprite extends Sprite implements RuleScriptedClass {}
+
+@:forceOverride class ScriptOpenFLTextField extends TextField implements RuleScriptedClass {}
+
+// ALE Psych
+
 class ScriptLuaPresetBase extends LuaPresetBase implements RuleScriptedClass {}
-
-@:forceOverride
-class ScriptOpenFLSprite extends Sprite implements RuleScriptedClass {}
-
-@:forceOverride
-class SciptOpenFLTextField extends TextField implements RuleScriptedClass {}
